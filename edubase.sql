3bda5bf67af07a392c854ec1c9674c08a19ed37c
Kanishk Malhotra <malhotra.kanishk0803@gmail.com>
2023-05-18 21:17:39 +0530

    connected to python

--- edubase.sql
+++ edubase.sql
@@ -1,66 +1,68 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: May 18, 2023 at 02:53 PM
--- Server version: 8.0.31
--- PHP Version: 8.0.26
+-- -- phpMyAdmin SQL Dump
+-- -- version 5.2.0
+-- -- https://www.phpmyadmin.net/
+-- --
+-- -- Host: 127.0.0.1:3306
+-- -- Generation Time: May 18, 2023 at 02:53 PM
+-- -- Server version: 8.0.31
+-- -- PHP Version: 8.0.26
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+-- START TRANSACTION;
+-- SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+-- /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+-- /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+-- /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `edubase`
---
+-- --
+-- -- Database: `edubase`
+-- --
 
--- --------------------------------------------------------
+-- -- --------------------------------------------------------
 
---
--- Table structure for table `attendance`
---
+-- --
+-- -- Table structure for table `attendance`
+-- --
 
-DROP TABLE IF EXISTS `attendance`;
-CREATE TABLE IF NOT EXISTS `attendance` (
-  `roll_no` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`roll_no`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- CREATE Database "edubase"
 
--- --------------------------------------------------------
+-- DROP TABLE IF EXISTS `attendance`;
+-- CREATE TABLE IF NOT EXISTS `attendance` (
+--   `roll_no` int NOT NULL AUTO_INCREMENT,
+--   `name` varchar(100) NOT NULL,
+--   PRIMARY KEY (`roll_no`)
+-- ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Table structure for table `marks`
---
+-- -- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `marks`;
-CREATE TABLE IF NOT EXISTS `marks` (
-  `roll_no` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`roll_no`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- --
+-- -- Table structure for table `marks`
+-- --
 
--- --------------------------------------------------------
+-- DROP TABLE IF EXISTS `marks`;
+-- CREATE TABLE IF NOT EXISTS `marks` (
+--   `roll_no` int NOT NULL AUTO_INCREMENT,
+--   `name` varchar(100) NOT NULL,
+--   PRIMARY KEY (`roll_no`)
+-- ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Table structure for table `submission`
---
+-- -- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `submission`;
-CREATE TABLE IF NOT EXISTS `submission` (
-  `roll_no` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`roll_no`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-COMMIT;
+-- --
+-- -- Table structure for table `submission`
+-- --
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- DROP TABLE IF EXISTS `submission`;
+-- CREATE TABLE IF NOT EXISTS `submission` (
+--   `roll_no` int NOT NULL AUTO_INCREMENT,
+--   `name` varchar(100) NOT NULL,
+--   PRIMARY KEY (`roll_no`)
+-- ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- COMMIT;
+
+-- /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+-- /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+-- /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
