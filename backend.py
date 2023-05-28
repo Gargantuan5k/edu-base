1b1c73290507fdbc9bd3ce1598c3173881b37638
Kanishk Malhotra <malhotra.kanishk0803@gmail.com>
2023-05-28 21:46:46 +0530

    added attendance marking

--- backend.py
+++ backend.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from main import tdy_date
+from main import tdy_date, markedAttendance, Name
 
 # Establish a connection to the MySQL database
 db = mysql.connector.connect(
@@ -12,11 +12,23 @@ db = mysql.connector.connect(
 # Create a cursor object to execute SQL queries
 cursor = db.cursor()
 
-# Write your SQL query to add a new column to the table
-alter_query = f"ALTER TABLE attendance ADD COLUMN `{tdy_date}` varchar(20)"
+# Check if the date column already exists in the table
+check_query = f"SHOW COLUMNS FROM attendance LIKE '{tdy_date}'"
+cursor.execute(check_query)
+result = cursor.fetchone()
+
+# Add the date as column to the table if it doesn't already exist
+if result is None:
+    alter_query = f"ALTER TABLE attendance ADD COLUMN `{tdy_date}` varchar(20)"
+    
+    # Execute the SQL query
+    cursor.execute(alter_query)
+
+#add attendance of specific student
+attendance_query = f"UPDATE attendance SET `{tdy_date}` = `{markedAttendance}` WHERE name = `{Name}`"
 
 # Execute the SQL query
-cursor.execute(alter_query)
+cursor.execute(attendance_query)
 
 # Commit the changes to the database
 db.commit()
