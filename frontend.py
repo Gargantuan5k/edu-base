21b2973d609461bc6090b55980adf3baac1f084f
sid <siddharth.vivek2000@outlook.com>
2023-06-02 14:38:24 +0530

    Add more student list interactions
    1. Modify student list: add/delete individual stu, change name
    2. Remove index of unnecessary file test.py

--- frontend.py
+++ frontend.py
@@ -16,23 +16,76 @@ def print_menu():
 
 
 def handle_students_list(action='create'):
-    students = {}
-    while True:
-        try:
-            num_stu = int(input("Enter the number of students in your classroom: "))
+    if action == 'create':
+        students = {}
+        while True:
+            try:
+                num_stu = int(input("Enter the number of students in your classroom: "))
 
-            for i in range(num_stu):
-                students[i+1] = input(f"Enter NAME of student, ROLL NUMBER {i+1}: ")
+                for i in range(num_stu):
+                    students[i+1] = input(f"Enter NAME of student, ROLL NUMBER {i+1}: ")
 
-            add_students(stu_dict=students)
-        except:
-            print('Invalid data entered or DB Error! Try again.')
-            continue
-        break
+                create_list(stu_dict=students)
+            except:
+                print('Invalid data entered! Try again.')
+                continue
+            break
 
-        
-    print('Students Successfully Added! You can modify your class list at any time through the same menu.')
-    run_menu()
+            
+        print('List Successfully Created! You can modify your class list at any time through the same menu.')
+        run_menu()
+
+    elif action == 'modify':
+        exists = check_src_exists()
+        if not exists:
+            print("No student list exists! Please create one to get started.")
+            run_menu()
+
+        while True:
+            ch = int(input('Press 1 to delete individual students, 2 to add individual students, 3 to change names, 4 to go back: '))
+            if ch == 1: # delete stu
+                while True:
+                    roll = int(input('Enter roll number to delete, enter 0 to stop.: '))
+                    if roll == 0:
+                        break
+
+                    res = delete_student(roll_no=roll)
+                    if not res:
+                        print(f"Roll number {roll} does not exist!")
+                    else:
+                        print(f"Deleted student {res}")
+            
+            elif ch == 2: # add stu
+                while True:
+                    roll = int(input("Enter new roll number, 0 to stop.: "))
+                    if roll == 0:
+                        break
+                    name = input("Enter new name: ")
+                    
+                    if check_student_rec_exists(roll):
+                        yn = input("WARNING: A student with this roll number already exists. Overwrite? (y/N): ").lower().strip()
+                        if yn != 'y':
+                            continue
+                        else:
+                            res = add_student(roll_no=roll, name=name, exists=True)
+                    else:
+                        res = add_student(roll_no=roll, name=name)
+
+                    print(f"Added new student {res}")
+
+
+            elif ch == 3: # change names
+                while True:
+                    roll = int(input('Enter roll number to change name, enter 0 to stop.: '))
+                    if roll == 0:
+                        break
+                    new_name = input(f"Enter new name for roll no. {roll}: ")
+                    
+                    res = update_student(roll_no=roll, new_name=new_name)
+                    print(f"Changed student details: {res['original']} -> {res['new']}")
+            else:
+                return
+
 
 def run_menu():
     print_menu()
@@ -48,7 +101,7 @@ def run_menu():
         ch1 = int(input("Choose an action: "))
 
         if ch1 == 1: # Create new list
-            exists = check_students_exists()
+            exists = check_src_exists()
             if exists:
                 ch2 = input('WARNING: Looks like a student list already exists! Creating/uploading a new list will overwrite the existing one. Proceed?(y/N): ').lower().strip()
                 if ch2 != 'y':
@@ -57,6 +110,7 @@ def run_menu():
 
         elif ch1 == 2: # Modify an existing list TODO
             handle_students_list(action='modify')
+
         elif ch1 == 3: # Visualise the existing list TODO
             handle_students_list(action='visualise')
         elif ch1 == 4: # Back
--- queries.py
+++ queries.py
@@ -15,18 +15,32 @@ def init_db():
     return db, cursor
 
 
-def check_students_exists():
-    _, cursor = init_db()
+def check_src_exists():
+    db, cursor = init_db()
     query = f"select roll_no, name from students_src"
     cursor.execute(query)
-    res = cursor.fetchone()
+    res = cursor.fetchall()
+
+    cursor.close()
+    db.close()
     return res is not None
 
 
-def add_students(**kwargs):
+def check_student_rec_exists(roll):
+    db, cursor = init_db()
+    query = f"select name from students_src where roll_no = '{roll}'"
+    cursor.execute(query)
+    res = cursor.fetchall()
+
+    cursor.close()
+    db.close()
+    return bool(res)
+
+
+def create_list(**kwargs):
     db, cursor = init_db()
     
-    if check_students_exists():
+    if check_src_exists():
         cursor.execute('delete from students_src')
 
     students = kwargs['stu_dict']
@@ -40,13 +54,68 @@ def add_students(**kwargs):
     db.close()
 
 
+def add_student(roll_no, name, exists=False):
+    db, cursor = init_db()
+
+    if exists:
+        cursor.execute(f"delete from students_src where roll_no = '{roll_no}'")
+    
+    add_query = f"insert into students_src(roll_no, name) values('{roll_no}', '{name}')"
+    cursor.execute(add_query)
+
+    db.commit()
+    cursor.close()
+    db.close()
+    return (roll_no, name)
+
+
+def delete_student(roll_no):
+    db, cursor = init_db()
+
+    exists_query = f"select * from students_src where roll_no = '{roll_no}'"
+    cursor.execute(exists_query)
+    res = cursor.fetchall()
+    if not res:
+        return False
+    
+    get_query = f"select * from students_src where roll_no = '{roll_no}'"
+    cursor.execute(get_query)
+    res = cursor.fetchall()
+    del_query = f"delete from students_src where roll_no = '{roll_no}'"    
+    cursor.execute(del_query)
+
+    db.commit()
+    cursor.close()
+    db.close()
+
+    return res
+
+
+def update_student(roll_no, new_name):
+    db, cursor = init_db()
+    res = {}
+
+    get_orig_query = f"select * from students_src where roll_no = '{roll_no}'"
+    cursor.execute(get_orig_query)
+    orig = cursor.fetchall()
+    res['original'] = orig
+
+    update_query = f"update students_src set name = '{new_name}' where roll_no = '{roll_no}'"
+    cursor.execute(update_query)
+    res['new'] = (roll_no, new_name)
+
+    db.commit()
+    cursor.close()
+    db.close()
+    return res
+
 def add_date(tdy_date):
     db, cursor = init_db()
 
     # Check if the date column already exists in the table
     check_query = f"SHOW COLUMNS FROM attendance LIKE '{tdy_date}'"
     cursor.execute(check_query)
-    result = cursor.fetchone()
+    result = cursor.fetchall()
 
     # Add the date as column to the table if it doesn't already exist
     if result is None:
@@ -64,7 +133,7 @@ def get_name(r_no):
     # Get student name, give prompt for att
     getName_query = f"SELECT name FROM attendance WHERE roll_no = {r_no}"
     cursor.execute(getName_query)
-    getName = cursor.fetchone()
+    getName = cursor.fetchall()
     print("Enter attendance for " + getName[0])
 
     db.commit()
