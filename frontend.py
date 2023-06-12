f6cc188571cc06b6315e90c13c94d3b0e36c80b0
sid <siddharth.vivek2000@outlook.com>
2023-06-12 21:47:23 +0530

    Fix issue #1
    Add data validation and error handling, minor ui cleanup

--- frontend.py
+++ frontend.py
@@ -39,52 +39,66 @@ def handle_students_list(action='create'):
 
     elif action == 'modify':
         while True:
-            ch = int(input('Press 1 to delete individual students, 2 to add individual students, 3 to change names, 4 to go back: '))
-            if ch == 1: # delete stu
+            ch = input('Press 1 to delete individual students, 2 to add individual students, 3 to change names, 4 to go back: ')
+            if ch == '1': # delete stu
                 while True:
-                    roll = int(input('Enter roll number to delete, enter 0 to stop.: '))
-                    if roll == 0:
-                        break
+                    try:
+                        roll = int(input('Enter roll number to delete, enter 0 to stop.: '))
+                        if roll == 0:
+                            break
 
-                    res = src_queries.delete_student(roll_no=roll)
-                    if not res:
-                        print(f"Roll number {roll} does not exist!")
-                    else:
-                        print(f"Deleted student {res}")
-            
-            elif ch == 2: # add stu
-                while True:
-                    roll = int(input("Enter new roll number, 0 to stop.: "))
-                    if roll == 0:
-                        break
-                    name = input("Enter new name: ")
-                    
-                    if src_queries.check_student_rec_exists(roll):
-                        yn = input("WARNING: A student with this roll number already exists. Overwrite? (y/N): ").lower().strip()
-                        if yn != 'y':
-                            continue
+                        res = src_queries.delete_student(roll_no=roll)
+                        if not res:
+                            print(f"Roll number {roll} does not exist!")
                         else:
-                            res = src_queries.add_student(roll_no=roll, name=name, exists=True)
-                    else:
-                        res = src_queries.add_student(roll_no=roll, name=name)
-
-                    print(f"Added new student {res}")
-
-
-            elif ch == 3: # change names
+                            print(f"Deleted student {res}")
+                    except (ValueError, TypeError):
+                        print('Invalid data entered!')
+                        continue
+            
+            elif ch == '2': # add stu
                 while True:
-                    roll = int(input('Enter roll number to change name, enter 0 to stop.: '))
-                    if roll == 0:
-                        break
-                    new_name = input(f"Enter new name for roll no. {roll}: ")
-                    
-                    res = src_queries.update_student(roll_no=roll, new_name=new_name)
-                    print(f"Changed student details: {res['original']} -> {res['new']}")
+                    try:
+                        roll = int(input("Enter new roll number, 0 to stop.: "))
+                        if roll == 0:
+                            break
+                        name = input("Enter new name: ")
+                        
+                        if src_queries.check_student_rec_exists(roll):
+                            yn = input("WARNING: A student with this roll number already exists. Overwrite? (y/N): ").lower().strip()
+                            if yn != 'y':
+                                continue
+                            else:
+                                res = src_queries.add_student(roll_no=roll, name=name, exists=True)
+                        else:
+                            res = src_queries.add_student(roll_no=roll, name=name)
 
-            else:
+                        print(f"Added new student {res}")
+                    except (ValueError, TypeError):
+                        print('Invalid data entered!')
+                        continue
+
+
+            elif ch == '3': # change names
+                while True:
+                    try:
+                        roll = int(input('Enter roll number to change name, enter 0 to stop.: '))
+                        if roll == 0:
+                            break
+                        new_name = input(f"Enter new name for roll no. {roll}: ")
+                        
+                        res = src_queries.update_student(roll_no=roll, new_name=new_name)
+                        print(f"Changed student details: {res['original']} -> {res['new']}")
+                    except (ValueError, TypeError):
+                        print('Invalid data entered!')
+                        continue
+
+            elif ch == '4':
                 return
+            else:
+                print('Invalid data entered!')
+                continue
 
-            return
     
     elif action == 'delete':
         src_queries.delete_list()
@@ -141,13 +155,17 @@ def handle_attendance(tdy_date=tdy_date, action='roll'):
 
         while True:
             while True:
-                rno = int(input('Enter roll number (or 0 to quit): '))
-                if rno == 0:
-                    return
-                exists = src_queries.check_student_rec_exists(rno)
-                if exists:
-                    break
-                print(f"Student with roll number {rno} does not exist!")
+                try:
+                    rno = int(input('Enter roll number (or 0 to quit): '))
+                    if rno == 0:
+                        return
+                    exists = src_queries.check_student_rec_exists(rno)
+                    if exists:
+                        break
+                    print(f"Student with roll number {rno} does not exist!")
+                except (ValueError, TypeError):
+                    print('Invalid data entered!')
+                    continue
         
             while True:
                 att = input(f"Enter attendance for Roll {rno} (p/a): ").lower().strip()
@@ -156,6 +174,7 @@ def handle_attendance(tdy_date=tdy_date, action='roll'):
                 elif att == 'a':
                     absent.append(rno)
                 else:
+                    print('Invalid data entered!')
                     continue
                 break
             
@@ -163,59 +182,66 @@ def handle_attendance(tdy_date=tdy_date, action='roll'):
 
     elif action == 'visualise': pass # TODO
 
-def run_menu():
+def run_menu(print_options=True):
     while True:
-        print_menu()
-        ch = int(input("Enter choice: "))
-
+        if print_options: # so that entire menu does not get re-printed every tme user enters invalid input on the main page.
+            print_menu()
+        else:
+            print_options = True
+        ch = input("Enter choice: ")
         # Program:
-        if ch == 1:
+        if ch == '1':
             print("-----------------")
             print("1. Create a new Students list")
             print("2. Update/modify my current Students List")
             print("3. View my Students List as text or a file")
             print("4. Delete my Students List")
             print("5. Go Back")
-            ch1 = int(input("Choose an action: "))
 
-            if ch1 == 1: # Create new list
-                exists = src_queries.check_src_exists()
-                if exists:
-                    ch2 = input('WARNING: Looks like a student list already exists! Creating/uploading a new list will overwrite the existing one. Proceed?(y/N): ').lower().strip()
-                    if ch2 != 'y':
-                        continue
-                handle_students_list(action='create')
-                continue
+            while True:
+                ch1 = input("Choose an action: ")
 
-            elif ch1 == 2: # modify existing list
-                exists = src_queries.check_src_exists()
-                if not exists:
-                    print("No student list exists! Please create one to get started.")
-                    continue
-                else:
-                    handle_students_list(action='modify')
+                if ch1 == '1': # Create new list
+                    exists = src_queries.check_src_exists()
+                    if exists:
+                        ch2 = input('WARNING: Looks like a student list already exists! Creating/uploading a new list will overwrite the existing one. Proceed?(y/N): ').lower().strip()
+                        if ch2 != 'y':
+                            continue
+                    handle_students_list(action='create')
                     continue
 
-            elif ch1 == 3: # Visualise the existing list TODO
-                handle_students_list(action='visualise')
-
-            elif ch1 == 4: # delete entire list
-                exists = src_queries.check_src_exists()
-                if not exists:
-                    print("No student list exists!")
-                    continue
-                else:
-                    yn = input("WARNING: Delete the students list? This action cannot be undone. (y/N): ").lower().strip()
-                    if yn == 'y':
-                        handle_students_list(action='delete')
+                elif ch1 == '2': # modify existing list
+                    exists = src_queries.check_src_exists()
+                    if not exists:
+                        print("No student list exists! Please create one to get started.")
                         continue
                     else:
+                        handle_students_list(action='modify')
                         continue
 
-            elif ch1 == 5: # Back
-                continue
+                elif ch1 == '3': # Visualise the existing list TODO
+                    handle_students_list(action='visualise')
 
-        if ch == 2:
+                elif ch1 == '4': # delete entire list
+                    exists = src_queries.check_src_exists()
+                    if not exists:
+                        print("No student list exists!")
+                        continue
+                    else:
+                        yn = input("WARNING: Delete the students list? This action cannot be undone. (y/N): ").lower().strip()
+                        if yn == 'y':
+                            handle_students_list(action='delete')
+                            continue
+                        else:
+                            continue
+
+                elif ch1 == '5': # Back
+                    break
+                else:
+                    print('Invalid data entered!')
+                    continue
+
+        if ch == '2':
             print(f'By default, this will edit the attendance for TODAY {str(date.today())}. If you would like to update attendance for another date, enter it below, or leave it blank to use the default.')
             user_date = input('Enter Date (FORMAT: YYYY-MM-DD): ').strip()
             ymd = user_date.split('-')
@@ -230,42 +256,49 @@ def run_menu():
             print("2. Mark attendance of specific student(s)")
             print("3. Get attendance report")
             print("4. Go back")
-            ch1 = int(input("Enter choice: "))
+            while True:
+                ch1 = input("Enter choice: ")
 
-            if ch1 == 1:
-                handle_attendance(action='roll', tdy_date=tdy_date)
-            
-            elif ch1 == 2:
-                handle_attendance(action='individual', tdy_date=tdy_date)
-            
-            elif ch1 == 3:
-                handle_attendance(action='visualise', tdy_date=tdy_date) # TODO
+                if ch1 == '1':
+                    handle_attendance(action='roll', tdy_date=tdy_date)
+                
+                elif ch1 == '2':
+                    handle_attendance(action='individual', tdy_date=tdy_date)
+                
+                elif ch1 == '3':
+                    handle_attendance(action='visualise', tdy_date=tdy_date) # TODO
 
-            elif ch1 == 4: # back
-                continue
+                elif ch1 == '4': # back
+                    break
+                else:
+                    print('Invalid data entered!')
+                    continue
 
-        elif ch == 3:
+        elif ch == '3':
             print("Enter choice: ")
             print("1. Add marks")
             print("2. Add student")
             print("3. Remove student")
             print("4. Go back")
 
-        elif ch == 4:
+        elif ch == '4':
             print("Enter choice: ")
             print("1. Add Submission")
             print("2. Add student")
             print("3. Remove student")
             print("4. Go back")
 
-        elif ch == 5:
-            rNo = int(input("Show report card of which roll no.?: "))
+        elif ch == '5':
+            # rNo = int(input("Show report card of which roll no.?: "))
+            pass
 
-        elif ch == 6:
+        elif ch == '6':
             break
 
         else:
-            print("Please enter valid choice")
+            print("Invalid data entered!")
+            print_options = False # do not reprint the menu; user has entered invalid data.
+            continue
 
 if __name__ == '__main__':
     run_menu()
